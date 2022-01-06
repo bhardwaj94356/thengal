f2a371a67a36225e25830ccdee88e6eb60985674
bharwdaj94356 <bhardwaj94356@gmail.com>
2022-01-06 15:19:22 +0530

    06-01-2022 15:19

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+function addNum() {
+    var a = document.getElementById("num1").value || 0;
+    var b = document.getElementById("num2").value || 0;
+
+    var c = parseInt(a) + parseInt(b);
+
+    document.getElementById("num3").value = c;
+}
+function dark() {
+    var notun = document.getElementById("theme");
+    notun.classList.toggle("dark-mode");
+}
